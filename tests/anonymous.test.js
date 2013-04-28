f05895e39dbd41e3ccc75dd6d47683320ea7fd97
AWinterman <andywinterman@gmail.com>
2013-04-28 15:48:12 -0700

    removed outdated tests, don't worry, I'll add them back someday

--- tests/anonymous.test.js
+++
@@ -1,4 +0,0 @@
-anonymous = require("../anonymous")
-
-anonymous(["tests/files/example.txt"], "a test to see if the thing works")
-anonymous(["tests/files/example.txt", "tests/files/example.md"], "multiple files")
--- tests/files/example.md
+++
@@ -1,8 +0,0 @@
-# Example #
-a work of ficiton is a wonderful thing
-
-```
-a small piece of  code
-is my preferential way
-to communicate
-```
