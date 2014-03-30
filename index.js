e9a0edd5dbb91856fae6b9b75273e07daf66095c
philip pedruco <philip.pedruco@gmail.com>
2014-03-30 13:53:12 +1100

    removed console.log's

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("This seems to have worked")
 module.exports.post = require("./lib/post")
 module.exports.oauth = require("./lib/OAuth")
 module.exports.credential = require("./lib/credential")
